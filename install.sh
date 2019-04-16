359a7e8b66b6e9f05525228e30134c972608440b
jan <niemantsverdriet@contentpower.nl>
2019-04-16 10:30:28 +0200

    alle datasets zitten er in

--- install.sh
+++ install.sh
@@ -19,3 +19,11 @@ cd ~
 # git checkout
 git clone https://github.com/niemantsverdriet/Verdi.git
 
+# admin user aan de database toevoegen
+mongo < ~/Verdi/setup.js
+
+# data importeren
+mongoimport --db ROS --collection system__datamodels --file ~/Verdi/installation/start-system__datamodels.json
+mongoimport --db ROS --collection system__fields --file ~/Verdi/installation/start-system__fields.json
+mongoimport --db ROS --collection system__apps --file ~/Verdi/installation/start-system__apps.json
+mongoimport --db ROS --collection system__users --file ~/Verdi/installation/start-system__users.json
