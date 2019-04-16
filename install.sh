9907e31d1fe78d2d0e34f6f156ed4dc2ad569830
jan <niemantsverdriet@contentpower.nl>
2019-04-16 09:13:26 +0200

    Git checkout aan installatie toegevoegd

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ apt-get install mongodb -y
 
 # mongodb starten
 service mongodb start
+
+# git installeren
+apt-get update && apt-get install git -y
+
+# map aanmaken
+cd ~
+mkdir Verdi
+cd Verdi
+
+# git checkout
+git clone https://github.com/niemantsverdriet/Verdi.git
