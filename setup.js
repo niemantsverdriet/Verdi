d553e76a6e7a2f35f2de9ee90bbf4d1dad5d2c46
jan <niemantsverdriet@contentpower.nl>
2019-04-16 16:28:18 +0200

    In installatie gebruiker voor ROS db toegevoegd

--- setup.js
+++ setup.js
@@ -6,3 +6,11 @@ db.createUser(
     roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
   }
 )
+use ROS
+db.createUser(
+    {
+      user: "admin",
+      pwd: "UevKCJHxcXcJ7BYA",
+      roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "dbOwner" ]
+    }
+  )
